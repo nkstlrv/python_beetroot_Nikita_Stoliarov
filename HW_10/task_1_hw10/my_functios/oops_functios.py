9575baa501babe6107a2cc21b2be164724b00bff
nkstlrv <stlrv_nk@proton.me>
2023-01-13 12:33:45 +0200

    Homework 10 uploaded

---
+++ HW_10/task_1_hw10/my_functios/oops_functios.py
@@ -0,0 +1,27 @@
+
+def oops():
+    usr_input_1 = input("\n Create your list"
+                        "\n ENTER numbers separated by coma: ")
+    usr_list = usr_input_1.split(",")
+    print(usr_list)
+
+    # Try to enter index out of range to call an IndexError
+    usr_input_2 = int(input("\n Now, ENTER an index of an item in a list to see what it is: "))
+    return print(usr_list[usr_input_2])
+
+
+def try_oops():
+    try:
+        oops()
+    except IndexError:
+        print("\n ERROR"
+              "\n Your index is out of list's range")
+    # raise KeyError
+
+
+# _____________________________________________________
+# Functions test:
+
+if __name__ == "__main__":
+    # oops()
+    try_oops()
