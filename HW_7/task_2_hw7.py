ac93b8cf0e0ad64ef94820e521fda6bc7aa7b978
nkstlrv <stlrv_nk@proton.me>
2023-01-15 10:57:09 +0200

    task_2_hw7 improved

--- HW_7/task_2_hw7.py
+++ HW_7/task_2_hw7.py
@@ -29,25 +29,42 @@ stock = {
 prices = {
     "banana": 4,
     "apple": 2,
+    "pear": 3,
     "orange": 1.5,
-    "pear": 3
+
 }
 
-stock_list = []
-for s_val in stock.values():
-    stock_list.append(float(s_val))
-print("\n Our item's quantities list is -->", stock_list)
+total_stock_price = 0
 
-prices_list = []
-for p_val in prices.values():
-    prices_list.append(float(p_val))
-print("\n Our item's prices list is -->", prices_list)
+items_list = [s for s in stock.keys()]
+print("\n List of our items in the stock -->", items_list)
 
-set_prices_list = []
-for i in range(len(stock_list)):
-    set_prices_list.append(stock_list[i] * prices_list[i])
-print("\n Price of each set of items  -->", set_prices_list)
+for item in items_list:
+    total_stock_price += stock[item] * prices[item]
 
-total_price = sum(set_prices_list)
+print("\n Answer is-->", total_stock_price)
 
-print("\n Our stock total price is -->", total_price)
+# --------------------------------------------------------------------------
+# Old solution:
+#
+# stock_list = []
+# for s_val in stock.values():
+#     stock_list.append(float(s_val))
+# print("\n Our item's quantities list is -->", stock_list)
+#
+# prices_list = []
+# for p_val in prices.values():
+#     prices_list.append(float(p_val))
+# print("\n Our item's prices list is -->", prices_list)
+#
+# set_prices_list = []
+# for i in range(len(stock_list)):
+#     set_prices_list.append(stock_list[i] * prices_list[i])
+# print("\n Price of each set of items  -->", set_prices_list)
+#
+#
+# total_price = sum(set_prices_list)
+#
+# print("\n Our stock total price is -->", total_price)
+#
+#
