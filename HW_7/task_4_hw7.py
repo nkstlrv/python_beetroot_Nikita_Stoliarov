1bad2e5a1f44c87be405d1a846a78cff74cdb7a4
nkstlrv <stlrv_nk@proton.me>
2023-01-15 11:04:55 +0200

    task_4_hw7 improved

--- HW_7/task_4_hw7.py
+++ HW_7/task_4_hw7.py
@@ -9,23 +9,35 @@ import calendar
 day_names = list(calendar.day_name)
 print("\n List of the week days -->", day_names)
 
-week_list_1 = [[d+1, i] for d, i in enumerate(day_names)]
+week_list = [d for d in day_names]
 
-dict_1 = {}
-dict_2 = {}
+week_dict_one = {}
 
-i_1 = 0
-i_2 = 0
+for ind, item in enumerate(week_list):
+    week_dict_one.update({ind: item})
+print("\n The first variant of dict -->", week_dict_one)
 
-while len(week_list_1) > i_1:
-    dict_1.update({week_list_1[i_1][0]: week_list_1[i_1][1]})
-    i_1 += 1
-print("\n Our first dict -->", dict_1)
+week_dict_two = {}
 
+for ind, item in enumerate(week_list):
+    week_dict_two.update({item: ind})
+print("\n The second variant of dict -->", week_dict_two)
 
-while len(week_list_1) > i_2:
-    dict_2.update({week_list_1[i_2][1]: week_list_1[i_2][0]})
-    i_2 += 1
-print("\n Our second dict -->", dict_2)
-
-
+# ----------------------------------------------------------------------------
+# Old solution:
+# dict_1 = {}
+# dict_2 = {}
+#
+# i_1 = 0
+# i_2 = 0
+#
+# while len(week_list_1) > i_1:
+#     dict_1.update({week_list_1[i_1][0]: week_list_1[i_1][1]})
+#     i_1 += 1
+# print("\n Our first dict -->", dict_1)
+#
+#
+# while len(week_list_1) > i_2:
+#     dict_2.update({week_list_1[i_2][1]: week_list_1[i_2][0]})
+#     i_2 += 1
+# print("\n Our second dict -->", dict_2)
