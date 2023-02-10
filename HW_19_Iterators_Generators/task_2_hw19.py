515172f47d64a1c4b222f93cebf6f78272aae2b2
nkstlrv <stlrv_nk@proton.me>
2023-02-10 20:53:28 +0200

    Task 2

---
+++ HW_19_Iterators_Generators/task_2_hw19.py
@@ -0,0 +1,22 @@
+
+class InRange:
+
+    def __init__(self, start, end, step=1):
+        self.value = start
+        self.end = end
+        self.step = step
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.value >= self.end:
+            raise StopIteration
+        value = self.value
+        self.value += self.step
+        return value
+
+
+for item in InRange(-5, 10, 5):
+    print(item)
+
