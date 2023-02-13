d0d6c00e7c78c36b76e14115f99ecdd19b106fb7
nkstlrv <stlrv_nk@proton.me>
2023-02-13 15:37:48 +0200

    Task 1 fixed

--- HW_21_Context_Manager/task_1_hw21.py
+++ HW_21_Context_Manager/task_1_hw21.py
@@ -2,26 +2,33 @@ class FileManager:
 
     instance_num = 0
 
-    def __init__(self, file_name, mode, text):
+    def __init__(self, file_name, mode):
         self.file_name = file_name
         self.mode = mode
-        self.text = text
 
     def __enter__(self):
         FileManager.instance_num += 1
-        return self
+        self.file = open(self.file_name, self.mode)
+        return self.file
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        with open(self.file_name, self.mode) as f:
-            f.write(f"Number of class instances --> {FileManager.instance_num}; Text --> {self.text}\n\n")
-            f.close()
+        self.file.close()
 
 
-with FileManager("task_1.txt", "w", "Some text") as f:
-    print("Executing my context manager")
+with FileManager("task_1.txt", "w") as f:
+    f.write(f"The number of class instances is --> {FileManager.instance_num}\n\n")
 
-with FileManager("task_1.txt", "a", "Some text") as f:
-    print("Executing my context manager")
+with FileManager("task_1.txt", "a") as f:
+    f.write(f"The number of class instances is --> {FileManager.instance_num}\n\n")
 
-with FileManager("task_1.txt", "a", "Some text") as f:
-    print("Executing my context manager")
+with FileManager("task_1.txt", "a") as f:
+    f.write(f"The number of class instances is --> {FileManager.instance_num}\n\n")
+
+print(f.closed)
+
+
+
+
+
+
+
